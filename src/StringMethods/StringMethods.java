cd858b1eb79e1bdfa31acf6823ce3515c3ebdd46
jscornick <56415689+jscornick@users.noreply.github.com>
2021-04-11 14:47:39 -0700

    StringMethods Complete

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,69 +32,121 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if (s1.length() > s2.length()) {
+			return s1;
+		} else if (s2.length() > s1.length()) {
+			return s2;
+		} else {
+			return s2;
+		}
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if (s.contains("underscores")) {
+			s = s.replace(' ', '_');
+		}
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		String[] alphabetical;
+		s1 = s1.trim();
+		s2 = s2.trim();
+		s3 = s3.trim();
+
+		String[] s1New = s1.split(" ");
+		String[] s2New = s2.split(" ");
+		String[] s3New = s3.split(" ");
+		if ((s1New[1].compareTo(s2New[1]) < 0) && s1New[1].compareTo(s3New[1]) < 0) {
+			alphabetical = s1New;
+		} else if (s2New[1].compareTo(s3New[1]) < 0 && s2New[1].compareTo(s1New[1]) < 0) {
+			alphabetical = s2New;
+		} else {
+			alphabetical = s3New;
+		}
+		return alphabetical[0] + " " + alphabetical[1];
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+				sum = sum + Integer.parseInt(s.charAt(i) + "");
+			}
+		}
+		return sum;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int counter = 0;
+		int location = s.indexOf(substring);
+		while (location != -1) {
+			counter++;
+			location = s.indexOf(substring, location + substring.length());
+		}
+		return counter;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		String key1 = key + "";
+		return Utilities.encrypt(s.getBytes(), key1.getBytes()[0]);
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		String key1 = key + "";
+		return Utilities.decrypt(s, key1.getBytes()[0]);
 	}
-
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+		int x = 0;
+		while (s.contains(substring + " ")) {
+			x++;
+			s = s.replaceFirst(substring + " ", "");
+		}
+		return x;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		int distance;
+		distance = s.lastIndexOf(substring) - (s.indexOf(substring) + substring.length());
+		return distance;
 	}
-
 
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
+		s = s.trim();
+		int x = 0;
+		int y = s.length() - 1;
+
+		while (y > x) {
+			if (s.charAt(x) != s.charAt(y)) {
+				return false;
+			}
+			x++;
+			y--;
+
+		}
 		return true;
+		}
 	}
-	
-}
+
 
 class Utilities {
 	// This basic encryption scheme is called single-byte xor. It takes a single
